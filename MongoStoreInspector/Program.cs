5bc129dc96c9db449ce4160f8eb09df6b36a4fa8
Sedward <sethaedwards@gmail.com>
2010-03-19 10:45:43 -0400

    Added the provider and Inspector console app. I also added new methods to SessionStore.cs

---
+++ MongoStoreInspector/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MongoStoreInspector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to dump the current session store or Q to quit");
+            Console.ReadLine();
+        }
+    }
+}
